9c6d29075f3e2e3b9b3de2e573c26e4d3123c2b0
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-25 12:32:47 +0000

    using Rcpp::populate
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@305 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,6 @@ NAMESPACE <- environment()
 MODULE <- Module( "parser_module" )
 
 .onLoad <- function( libname, pkgname ){
-	assign( "nlines", MODULE$nlines, NAMESPACE )
-	assign( "count.chars", MODULE$count.chars, NAMESPACE )
-	
-	lockBinding( "nlines", NAMESPACE )
-	lockBinding( "count.chars", NAMESPACE )
+	populate( MODULE, NAMESPACE )
 }
 
